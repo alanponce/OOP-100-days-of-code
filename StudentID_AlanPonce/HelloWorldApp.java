b7fb77b11ed9bf4e6b4d4324b81768b79366ac35
Alan Ponce <alanponc@hotmail.com>
2019-03-04 22:42:49 -0700

    Add files via upload

---
+++ StudentID_AlanPonce/HelloWorldApp.java
@@ -0,0 +1,18 @@
+
+/**
+ ** The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ *
+ *
+ * @author alanponce
+ *
+ *
+ */
+public class HelloWorldApp {
+	
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+        System.out.println("“First, solve the problem. ..."); // Display the string.
+        System.out.println("Happy coding"); // Display the string.
+    }
+}
