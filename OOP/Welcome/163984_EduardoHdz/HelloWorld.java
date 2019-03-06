c15370e0f5b8f01dfa5b3aed72cbdb941bbc2e6b
Eduardo Hernández <al163984@alumnos.uacj.mx>
2019-03-06 09:27:17 -0700

    Add files via upload

---
+++ OOP/Welcome/163984_EduardoHdz/HelloWorld.java
@@ -0,0 +1,13 @@
+/*
+ *
+ *
+ * @author Eduardo Hdz
+ *
+ *
+ */
+public class HelloWorld {
+	
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Manda un msj a pantalla
+    }
+}
