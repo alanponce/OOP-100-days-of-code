4c9459f0676e00ebc0233675c5233a5a66481a0d
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-03-06 23:28:52 -0700

    Add files via upload

---
+++ OOP/Welcome/163721_AnelizSantillán/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+  * Nombre: Aneliz Santillán 163721 
+  * Fecha: 06/03/2019
+  * Hora: 10:47 p.m.
+**/
+
+public class HelloWorld // Definición de una clase, llamada "HelloWorld"
+{ 
+   public static void main(String args[]) // Se define el método main
+  { 
+   System.out.println("\nHello\n   World\n       <3"); // Despliega un string en la consola. 
+                                      //Si una función regresara un texto sería String
+  }
+
+}
