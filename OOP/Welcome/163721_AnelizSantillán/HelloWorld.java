8d4423fc8cbb11b732abb7e42cab782bc4c3177e
163721Aneliz <46726319+163721aneliz@users.noreply.github.com>
2019-03-06 22:29:56 -0800

    Update HelloWorld.java

--- OOP/Welcome/163721_AnelizSantillán/HelloWorld.java
+++ OOP/Welcome/163721_AnelizSantillán/HelloWorld.java
@@ -1,15 +1,15 @@
 /**
-  * Nombre: Aneliz Santill·n 163721 
+  * Nombre: Aneliz Santill√°n 163721 
   * Fecha: 06/03/2019
   * Hora: 10:47 p.m.
 **/
 
-public class HelloWorld // DefiniciÛn de una clase, llamada "HelloWorld"
+public class HelloWorld // Definici√≥n de una clase, llamada "HelloWorld"
 { 
-   public static void main(String args[]) // Se define el mÈtodo main
+   public static void main(String args[]) // Se define el m√©todo main
   { 
    System.out.println("\nHello\n   World\n       <3"); // Despliega un string en la consola. 
-                                      //Si una funciÛn regresara un texto serÌa String
+                                                       //Si una funci√≥n regresara un texto ser√≠a String
   }
 
 }
