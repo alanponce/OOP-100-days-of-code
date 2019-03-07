b3860f6366dbe6cfea0eb89abf208a4c478028e9
al169818EdgarReyes <47508434+al169818EdgarReyes@users.noreply.github.com>
2019-03-06 22:04:49 -0700

    Add files via upload

---
+++ OOP/Welcome/169818_EdgarReyes/hello.java
@@ -0,0 +1,9 @@
+// Nombre: Edgar Omar Reyes Olivas	Hora: 9:57	Fecha: 03/06/19
+
+class Hello{	//Declaration of class Hello.
+	public static void main(String args[]){	//Declaration of main function.
+		System.out.println("Hello World!"); //Prints Hello World.
+	}
+}
+
+
