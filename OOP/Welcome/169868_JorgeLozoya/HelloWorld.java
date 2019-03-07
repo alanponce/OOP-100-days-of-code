26d68d6620dc697826072952b2c720df951d6b37
Jorge Lozoya - 169868 <43255434+JorgeLozoya169868@users.noreply.github.com>
2019-03-07 12:20:57 -0700

    Add files via upload

---
+++ OOP/Welcome/169868_JorgeLozoya/HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+* @Jorge Lozoya
+* @Timestart 12:00 PM 
+* @ 07/03/ 2019
+*/
+class HelloWorld{
+ 
+ public static void main(String[] args) {
+  System.out.println("Hello\nMy name is Jorge Lozoya!"); //System.out.println es la forma de imprimir en pantalla e imprime lo que está entrecomillado
+ }
+ 
+}
