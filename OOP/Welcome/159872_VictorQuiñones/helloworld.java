2a66aa0f83a4f503376900c258b7ceddf0477f9f
159872VictorQuinones <46726694+159872VictorQuinones@users.noreply.github.com>
2019-03-07 12:34:10 -0700

    Add files via upload

---
+++ OOP/Welcome/159872_VictorQuiñones/helloworld.java
@@ -0,0 +1,17 @@
+/**
+ ** The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ *
+ *
+ * @author Victor Manuel Quiñones Pineda 159872
+ *
+ *
+ */
+public class helloworld {
+	
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+	System.out.println("I am Victor Quiñones");
+        System.out.println("Accept the challenge and happy coding!!"); // Display the string.
+    }
+}
