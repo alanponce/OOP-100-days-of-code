fb2a82c694f69d2338e6543c6d23b158c6af9d23
169840 Hector Sanchez <46726559+169840HectorSanchez@users.noreply.github.com>
2019-03-07 12:30:35 -0700

    Add files via upload

---
+++ OOP/Welcome/169840_HectorSanchez/HelloWorld.java
@@ -0,0 +1,10 @@
+/*Nombre: Hector_Gerardo_Sanchez_Quiroga
+* Hora: 9:51
+* Fecha: 03/06/2019
+*/
+
+class HelloWorld{
+	public static void main(String[] args){
+		System.out.print("Hello World!!!");
+}
+}
