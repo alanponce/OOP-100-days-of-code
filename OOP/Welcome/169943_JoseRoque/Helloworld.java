89a2aae963c9944abd23fd3c8554bdda96950b4e
169943alejandroroque <46771407+alejandroroque@users.noreply.github.com>
2019-03-07 12:34:42 -0700

    Add files via upload

---
+++ OOP/Welcome/169943_JoseRoque/Helloworld.java
@@ -0,0 +1,9 @@
+// Nombre: Jose Alejandro Roque
+// Dia: 07/03/2019
+// Hora: 9:49am
+
+public class Helloworld{
+ public static void main(String [] args){
+ System.out.println("Hola mundo"); 
+}
+ }
