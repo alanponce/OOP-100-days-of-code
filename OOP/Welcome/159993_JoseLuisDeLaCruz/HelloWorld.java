670b380a5201022f1743b74fbd5a1190d5c90461
159993JoseLuisDeLaCruzG <46793870+JoseLuisDLG@users.noreply.github.com>
2019-03-07 21:41:05 -0700

    Add files via upload

---
+++ OOP/Welcome/159993_JoseLuisDeLaCruz/HelloWorld.java
@@ -0,0 +1,11 @@
+//Nombre: Jose Luis De La Cruz Gonzalez
+//Dia: 07/03/2019
+//Hora:07:59 pm
+
+public class HelloWorld{
+public static void main (String [] args){
+
+System.out.print("Hello World\n");
+
+}
+}
