bee9f4618f724f0db9136989d58d86cecd18bc90
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-11 17:50:46 -0700

    Update Supermercado.java

--- OOP/Welcome/169881_CarlosVillanueva/Proyecto_Supermercado/Supermercado.java
+++ OOP/Welcome/169881_CarlosVillanueva/Proyecto_Supermercado/Supermercado.java
@@ -1,4 +1,17 @@
+import java.util.Scanner;
+class Carnes{
+  private String Nombre;
+  private int Precio;
+  private int Cantidad;
+  Private int Total;
+}
 class Supermercado{
 public static void main(String[]args){
-
+System.out.println("Supermercado en línea");
+Carnes Chuleta = new Carnes();
+  Chuleta.nombre = "Chuleta de Cerdo";
+  Chuleta.Precio = 93;
+  Chuleta.Cantidad = 2;
+  Chuleta.Total = Chuleta.Precio*Chuleta.Cantidad;
+System.out.println(Chuleta.nombre+", Precio: "+Chuleta.Precio+", cantidad: "+Chuleta.Cantidad+" total: "+Chuleta.Total);
 }}
