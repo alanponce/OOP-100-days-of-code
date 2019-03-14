d7b5eb7226aad80f2f0c7b5e2f6df4affacd8802
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-13 19:31:18 -0700

    Update Supermercado.java

--- OOP/Students/169881_CarlosVillanueva/Proyecto_Supermercado/Supermercado.java
+++ OOP/Students/169881_CarlosVillanueva/Proyecto_Supermercado/Supermercado.java
@@ -1,7 +1,7 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
 //Día: 13/03/2019
 //Hora de inicio: 07:48
-//Hora de fin: 
+//Hora de fin: 08:30
 import java.util.Scanner;
 class Empleado{
   static String Nombre;
