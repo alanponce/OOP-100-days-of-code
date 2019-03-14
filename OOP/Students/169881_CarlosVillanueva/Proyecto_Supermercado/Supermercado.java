d03dc4ed220ad42595e4bc5e0f54f3ae8f00efb6
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-13 19:31:03 -0700

    Update Supermercado.java

--- OOP/Students/169881_CarlosVillanueva/Proyecto_Supermercado/Supermercado.java
+++ OOP/Students/169881_CarlosVillanueva/Proyecto_Supermercado/Supermercado.java
@@ -1,22 +1,30 @@
 //Nombre: Villanueva Aguilar Carlos Raymundo 
 //Día: 13/03/2019
 //Hora de inicio: 07:48
-//Hora de fin: 07:
+//Hora de fin: 
 import java.util.Scanner;
 class Empleado{
-  String Nombre;
-  String Puesto;
-  void Saludo(){
-    System.out.println("Hola, soy"+Nombre+" y hoy voy a ser su "+Puesto);
-}
+  static String Nombre;
+  static String Puesto;
+  public static void Saludo(){
+    System.out.println("Hola, soy "+Nombre+" y hoy voy a ser su "+Puesto);
+}}
 class Carnicero extends Empleado{
-  String Articulo;
-  int Precio;
-  int Cantidad;
-  int Total;
-  void Proceso(){
-    System.out.println("Aqui tiene su "+Articulo+", "+Cantidad+", Kg");
+   public Carnicero(){
+}
+   static String Articulo;
+   static int Precio;
+   static int Cantidad;
+   static int Total;
+   static void Proceso(){
+    System.out.println("Aqui tiene su "+Articulo+", "+Cantidad+" Kg");
   }
+}
+class Cajero extends Empleado{
+ static int Total;
+ static void Caja(){
+System.out.println("Van a ser "+Total+" pesos.");
+}
 }
 class Carnes{
   String Nombre;
@@ -24,20 +32,26 @@ class Carnes{
   int Cantidad;
   int Total;
 }
-  Carnes Chuleta = new Carnes();
-  Chuleta.Nombre = "Chuleta de Cerdo";
-  Chuleta.Precio = 93;
-  Chuleta.Cantidad = 2;
-  Chuleta.Total = Chuleta.Precio*Chuleta.Cantidad;
 class Supermercado{
 public static void main(String[]args){
 System.out.println("Carniceria del Supermercado");
-Carnicero.Nombre = "Juan";
-Carnicero.Puesto = "carnicero";
-Carnicero.Saludo();
+Carnes Chuleta = new Carnes();
+Chuleta.Nombre = "Chuleta de Cerdo";
+Chuleta.Precio = 93;
+Chuleta.Cantidad = 2;
+Chuleta.Total = Chuleta.Precio*Chuleta.Cantidad;
 Carnicero.Articulo = Chuleta.Nombre;
 Carnicero.Precio = Chuleta.Precio;
 Carnicero.Cantidad = Chuleta.Cantidad;
 Carnicero.Total = Chuleta.Total;
+Cajero.Total = Carnicero.Total;
+Cajero.Nombre = "Jorge";
+Cajero.Puesto = "cajero";
+Carnicero.Nombre = "Juan";
+Carnicero.Puesto = "carnicero";
+Carnicero.Saludo();//Error: El saludo se muestra como si estuviera hablando el cajero, debo investigar y corregir.
 Carnicero.Proceso();
+System.out.println("Cajas del Supermercado");
+Cajero.Saludo();
+Cajero.Caja();
 }}
