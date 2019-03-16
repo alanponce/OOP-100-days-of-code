95a117d9879aaa3f716d1148fbd95dfa1484648d
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-16 11:46:31 -0700

    Update Empleado.java

--- OOP/Proyects/Proyecto_Supermercado/Proyecto_Actualizado/Empleado.java
+++ OOP/Proyects/Proyecto_Supermercado/Proyecto_Actualizado/Empleado.java
@@ -1,18 +1,22 @@
 package proyecto.supermercado;
-class Empleado{//La clase empleado se implementa para simular atributos y comportamientos de un empleado de supermercado.
+public abstract class Empleado{//La clase empleado se implementa para simular atributos y comportamientos de un empleado de supermercado.
   //Se declaran los atributos de la clase Empleado, que pueden ser heredadas por otras clases.
   static String Nombre;
   static String Puesto;
-  //Se crea el método saludo, que puede ser heredado por otras clases.
-  public static void Saludo(){
-    System.out.println("Hola, soy "+Nombre+" y hoy voy a ser su "+Puesto);
-}}
+  //Se crea el método abstracto saludo, que puede ser heredado por otras clases.
+  public abstract void Saludo();
+      
+}
 class Carnicero extends Empleado{//La clase carnicero es heredada de la clase empleado para simular a un carnicero de supermercado.
    //Se declaran los atributos propios de la clase carnicero.
    static String Articulo;
    static int Precio;
    static int Cantidad;
    static int Total;
+   @Override
+   public void Saludo(){
+   System.out.println("Hola, soy "+Nombre+" y hoy voy a ser su "+Puesto);
+   }
   //Se crea el método proceso, el cual es propio de la clase Carnicero.
    public static void Proceso(){
     System.out.println("Aqui tiene su "+Articulo+", "+Cantidad+" Kg");
@@ -23,6 +27,10 @@ class Cajero extends Empleado{//La clase cajero es heredada de la clase empleado
  //Se declaran atributos propios de la clase cajero.
  static int Total;
  //Se crea el método caja, el cual es propio de la clase Cajero
+ @Override
+   public void Saludo(){
+   System.out.println("Hola, soy "+Nombre+" y hoy voy a ser su "+Puesto);
+   }
  static void Caja(){
 System.out.println("Van a ser "+Total+" pesos.");
 }
@@ -33,7 +41,7 @@ class Carnes{//La clase carnes es creada para simular los atributos de un produc
   int Precio;
   int Cantidad;
   int Total;
-  public Carnes(String Nombre, int Precio, int Cantidad){//Se crea un constructor para la clase Carnes
+  public Carnes(String Nombre, int Precio, int Cantidad){
       this.Nombre = Nombre;
       this.Precio = Precio;
       this.Cantidad = Cantidad;
