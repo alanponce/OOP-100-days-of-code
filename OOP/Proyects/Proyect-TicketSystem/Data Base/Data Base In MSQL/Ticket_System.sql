e60a9b1650bf667057136c22acbd82be8d22a7f9
159949IvanValdez <46763262+159949IvanValdez@users.noreply.github.com>
2019-03-16 11:40:14 -0600

    Add files via upload

--- OOP/Proyects/Proyect-TicketSystem/Data Base/Data Base In MSQL/Ticket_System.sql
+++ OOP/Proyects/Proyect-TicketSystem/Data Base/Data Base In MSQL/Ticket_System.sql
@@ -21,21 +21,55 @@ constraint Fk_DepartmentID foreign key (DepartmentID) references Department(Depa
 );
 
 create table Bus(
-Number_Plate varchar(7),
+Number_Plate varchar(7) not null,
 Driver varchar(25),
 Model varchar(25),
 constraint Pk_Number_Plate primary key (Number_Plate)
 );
 
 create table Trip(
-TripID int,
+TripID int not null,
 BusNumber_Plate varchar(7),
 TicketQTY tinyint,
 Departure_Day date,
 Time_Departure time,
 Destiny varchar(45),
 constraint Pk_TripID primary key (TripID),
-constraint Fk_BusNumber_Plate foreign key (BusNumber_Plate) references Bus (Number_Plate)
+constraint Fk_BusNumber_Plate foreign key (BusNumber_Plate) references Bus(Number_Plate)
 );
 
+create table Seat(
+Number varchar(2) not null,
+TripID int,
+SoldState tinyint,
+constraint Pk_Number primary key (Number,TripID),
+constraint Fk_TripID foreign key (TripID) references Trip(TripID)
+);
 
+create table Sales(
+SalesID int not null,
+Client_Name varchar(20),
+Client_Phone varchar(10),
+Date date,
+TicketsSold int,
+EmployeeID int,
+TripID int,
+Reservation bit,
+Amount_Received int,
+Total_Sale int,
+constraint Pk_SalesID primary key (SalesID),
+constraint Fk_EmployeeID foreign key (EmployeeID) references Employee(EmployeeID),
+constraint Fk_TripIDSales foreign key (TripID) references Trip(TripID)
+);
+
+Create table Ticket(
+TicketID int,
+SalesID int,
+SeatTripID int,
+Price double,
+SeatNumber varchar(2),
+constraint Pk_TicketIDCompuesta primary key(TicketID,SalesID,SeatTripID),
+constraint Fk_SalesID foreign key(SalesID) references Sales(SalesID),
+constraint Fk_SeatTripID foreign key(SeatTripID) references Seat(TripID),
+constraint Fk_SeatNumber foreign key(SeatNumber) references Seat(Number)
+);
