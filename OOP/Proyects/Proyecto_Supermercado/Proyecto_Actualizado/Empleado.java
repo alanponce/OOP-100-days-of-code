99ce744f89c630322e64c259f69be79b3b60442a
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-03-15 19:21:00 -0600

    Add files via upload

---
+++ OOP/Proyects/Proyecto_Supermercado/Proyecto_Actualizado/Empleado.java
@@ -0,0 +1,41 @@
+package proyecto.supermercado;
+class Empleado{//La clase empleado se implementa para simular atributos y comportamientos de un empleado de supermercado.
+  //Se declaran los atributos de la clase Empleado, que pueden ser heredadas por otras clases.
+  static String Nombre;
+  static String Puesto;
+  //Se crea el método saludo, que puede ser heredado por otras clases.
+  public static void Saludo(){
+    System.out.println("Hola, soy "+Nombre+" y hoy voy a ser su "+Puesto);
+}}
+class Carnicero extends Empleado{//La clase carnicero es heredada de la clase empleado para simular a un carnicero de supermercado.
+   //Se declaran los atributos propios de la clase carnicero.
+   static String Articulo;
+   static int Precio;
+   static int Cantidad;
+   static int Total;
+  //Se crea el método proceso, el cual es propio de la clase Carnicero.
+   public static void Proceso(){
+    System.out.println("Aqui tiene su "+Articulo+", "+Cantidad+" Kg");
+  }
+}
+
+class Cajero extends Empleado{//La clase cajero es heredada de la clase empleado para simular a un cajero de supermercado.
+ //Se declaran atributos propios de la clase cajero.
+ static int Total;
+ //Se crea el método caja, el cual es propio de la clase Cajero
+ static void Caja(){
+System.out.println("Van a ser "+Total+" pesos.");
+}
+}
+class Carnes{//La clase carnes es creada para simular los atributos de un producto de carne en el supermercado.
+  //Se declaran los atributos de la clase Carnes
+  String Nombre;
+  int Precio;
+  int Cantidad;
+  int Total;
+  public Carnes(String Nombre, int Precio, int Cantidad){
+      this.Nombre = Nombre;
+      this.Precio = Precio;
+      this.Cantidad = Cantidad;
+  }
+}
---
+++ OOP/Proyects/Proyecto_Supermercado/Proyecto_Actualizado/ProyectoSupermercado.java
@@ -0,0 +1,33 @@
+//Nombre: Villanueva Aguilar Carlos Raymundo 
+//Día: 15/03/2019
+//Hora de inicio: 18:00
+//Hora de fin: 20:10
+package proyecto.supermercado;
+import java.util.Scanner;
+public class ProyectoSupermercado {
+    public static void main(String[]args){
+Scanner N = new Scanner(System.in);
+String Salida;
+System.out.println("Carniceria del Supermercado");/*Por el momento se va a iniciar el programa usando la carnicería, por lo que se
+imprime en pantalla un texto que lo indica.*/
+Carnicero.Nombre = "Juan";//Se asigna el atributo de nombre de la clase carnicero como "Juan".
+Carnicero.Puesto = "carnicero";//Se asigna el atributo de puesto de la clase carnicero como "carnicero".
+Carnicero.Saludo();//Realiza el método Saludo de la clase Carnicero.
+Carnes Chuleta = new Carnes("Chuleta de Cerdo",92,0);//Se crea una chuleta, la cual es un objeto de la clase carnes.
+System.out.println("Va a llevar "+Chuleta.Nombre+" ¿Qué Cantidad?");
+Chuleta.Cantidad += N.nextInt();
+Carnicero.Articulo = Chuleta.Nombre;//Se asigna el atributo de artículo en la clase carnicero con el nombre que posee el objeto chuleta.
+Carnicero.Precio = Chuleta.Precio;//Se asigna el atributo de precio en la clase carnicero con el precio que posee el objeto chuleta.
+Carnicero.Cantidad = Chuleta.Cantidad;//Se asigna el atributo de cantidad en la clase carnicero con la cantidad que posee el objeto chuleta.
+Carnicero.Total = Chuleta.Precio*Chuleta.Cantidad;//Se asigna el atributo de precio total en la clase carnicero con el precio total que posee el objeto chuleta.
+Carnicero.Proceso();//Realiza el método Proceso de la clase Carnicero. 
+Cajero.Total = Carnicero.Total;//Se asigna el atributo de precio total en la clase cajero con el precio total que posee la clase carnicero.
+Cajero.Nombre = "Jorge";//Se asigna el atributo de nombre de la clase carnicero como "Jorge".
+Cajero.Puesto = "cajero";//Se asigna el atributo de puesto de la clase cajero como "cajero".
+System.out.println("Cajas del Supermercado");//Se imprime un mensaje que indica que ahora se encuentra en las cajas.
+Cajero.Saludo();//Realiza el método Saludo de la clase Cajero.
+Cajero.Caja();//Realiza el método Caja de la clase Cajero.
+/*Error solucionado: Al asignar y mostrar primero los atributos y métodos de la clase carnicero, y después asignar y mostrar los
+atributos y métodos de la clase cajero, se logra realizar el proceso como se esperaba; de todos modos, aún pienso que ésta práctica no
+es óptima, por lo que me dedicaré a investigar alguna manera de hacer esto de forma más efectiva y práctica.*/
+}}
