0e819494d66fbb0af007efda0788a4b3328b6e59
al169838DiegoAlmanza <48567250+al169838DiegoAlmanza@users.noreply.github.com>
2019-03-19 13:02:22 -0600

    Add files via upload

---
+++ OOP/Proyects/De C++ a Java/Conjuntos/Java/src/Conjunto.java
@@ -0,0 +1,39 @@
+﻿package conjunto;
+
+
+public class Conjunto {
+    
+    public static void main(String[] args) {
+       Crear_Conjunto a = new Crear_Conjunto();
+       Crear_Conjunto b = new Crear_Conjunto();
+       Crear_Conjunto c = new Crear_Conjunto();
+       
+       System.out.print("Conjunto 1 \n");
+        a.Insertar(2);
+        a.Insertar(9);
+        a.Insertar(6);
+        
+        a.imprimir();
+        
+        
+       System.out.print("\nConjunto 2\n");
+        b.Insertar(9);
+        b.Insertar(6);
+        b.Insertar(1);
+        
+        b.imprimir();
+       
+        
+        OperacionesConjuntos d = new OperacionesConjuntos();
+        
+        System.out.print("\nUnion de los Conjuntos: ");
+            d.Union(a, b);
+        
+        System.out.print("\nInterseccion de Conjuntos: ");
+            d.Interseccion(a, b);
+            
+               
+       
+    }
+    
+}
