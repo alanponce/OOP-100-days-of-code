fe3efdd62075091e4a13391b7c6d4c57d0d29388
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-03-20 16:27:10 -0600

    Update Conjunto.java

--- OOP/Proyects/De C++ a Java/Conjuntos/Java/src/Conjunto.java
+++ OOP/Proyects/De C++ a Java/Conjuntos/Java/src/Conjunto.java
@@ -1,14 +1,15 @@
-﻿package conjunto;
+﻿package conjunto;//creamos un package llamandolo conjunto en el que estaran todas las clases que se relacionan entre si
 
 
-public class Conjunto {
+public class Conjunto { //Creamos una clase y la llamamos Conjunto
     
-    public static void main(String[] args) {
+    public static void main(String[] args) {// creamos el main
+        //Creamos tres  conjuntos y los llamamos a,b,c respectivamente
        Crear_Conjunto a = new Crear_Conjunto();
        Crear_Conjunto b = new Crear_Conjunto();
        Crear_Conjunto c = new Crear_Conjunto();
        
-       System.out.print("Conjunto 1 \n");
+       System.out.print("Conjunto 1 \n");//imprime el primer conjunto de numeros
         a.Insertar(2);
         a.Insertar(9);
         a.Insertar(6);
@@ -16,7 +17,7 @@ public class Conjunto {
         a.imprimir();
         
         
-       System.out.print("\nConjunto 2\n");
+       System.out.print("\nConjunto 2\n");//imprime el segundo conjunto de numeros
         b.Insertar(9);
         b.Insertar(6);
         b.Insertar(1);
@@ -24,12 +25,12 @@ public class Conjunto {
         b.imprimir();
        
         
-        OperacionesConjuntos d = new OperacionesConjuntos();
+        OperacionesConjuntos d = new OperacionesConjuntos();//llamamos a la operacion entre conjuntos
         
-        System.out.print("\nUnion de los Conjuntos: ");
+        System.out.print("\nUnion de los Conjuntos: ");//nos imprime la union entre el conjunto a y b
             d.Union(a, b);
         
-        System.out.print("\nInterseccion de Conjuntos: ");
+        System.out.print("\nInterseccion de Conjuntos: ");//nos imprime la interseccion entre los conjuntos a y b
             d.Interseccion(a, b);
             
                
