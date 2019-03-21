8064d1f88704da0dc21c9b74443ebbf7fd09c7c1
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-03-21 11:51:09 -0600

    Update Conjunto.cpp

--- OOP/Proyects/De C++ a Java/Conjuntos/C++/Conjunto.cpp
+++ OOP/Proyects/De C++ a Java/Conjuntos/C++/Conjunto.cpp
@@ -1,55 +1,55 @@
-#include "Conjunto.h"//utilizamoe el include para adicionar el conjunto.h a nuestro codigo
+#include "Conjunto.h"//incluimos la clase conjunto
 #include <iostream>//incluimos la biblioteca standard iostream
 using namespace std;
 
-Conjunto::Conjunto()
+Conjunto::Conjunto() //funcion del constructor
 {
-	tam=0;//creamos un arreglo llamado tam y le damos un valor que es cero
+	tam=0;//el tamano se inicializa en cero
 }
 
-void Conjunto::Vaciar(){
-	tam=0;		
+void Conjunto::Vaciar(){ //funcion vaciar
+	tam=0; //tamano se inicializa en cero		
 }
 
-bool Conjunto::Pertenece(int x){
-	for(int i=0;i<tam;i++)
-		if(conjunto[i]==x)		
-			return true;			
+bool Conjunto::Pertenece(int x){ // este metodo inidica si el elemento x pertenece al conunto o no
+	for(int i=0;i<tam;i++) //creamos ciclo for
+		if(conjunto[i]==x) //definimos que entra al ciclo	
+			return true; //si entra al ciclo regresa verdadero			
 			
-	return false;
+	return false; //si no entra al ciclo regresa falso
 }
 
-void Conjunto::Insertar(int x){
-	if(!Pertenece(x)){
+void Conjunto::Insertar(int x){ // este metodo inserta el elemento x en el conjunto 
+	if(!Pertenece(x)){ 
 		conjunto[tam]=x;
-		tam++;
+		tam++; //existe un incremento en el tamano
 	}else
-		cout<<"No se pudo insertar "<< x <<" ya que ya existe en el conjunto "<<endl;		
+		cout<<"No se pudo insertar "<< x <<" ya que ya existe en el conjunto "<<endl;// si el elemento ya pertenece a nuestro conunto ya no lo inserta	
 }
 
-bool Conjunto::esVacio(){
-	if(tam==0)
-		return true;
+bool Conjunto::esVacio(){ //este metodo indica que el conjunto es vacio
+	if(tam==0) //si el tamano es igual a cero
+		return true;//si el tamano es igual a cero regresa verdadero
 	else
-		return false;
+		return false;//si el tamano es diferente de cero nos regresa un falso
 }
 
-int Conjunto::tamano(){
-	return tam;	
+int Conjunto::tamano(){ //funcion del tamano del conunto
+	return tam; //retorna el tamano
 }
 
-int Conjunto::Mostrar(int posicion){
-	if(posicion<0 || posicion>=tam){
-		cout<<"posicion "<< posicion <<" invalida!!!"<<endl<<endl;
-		return -1;
+int Conjunto::Mostrar(int posicion){ //funcion de mostrar la posicion del copnunto
+	if(posicion<0 || posicion>=tam){ //si la posicion es mayor a cero o la posicion es mayor o igual al tamano
+		cout<<"posicion "<< posicion <<" invalida!!!"<<endl<<endl;// 
+		return -1; //si la posicion es invalida regresa -1
 	}
-	return conjunto[posicion];
+	return conjunto[posicion];//si la posicion es valida regresa la posicion del conjunto
 }
 
-void Conjunto::imprimir(){
+void Conjunto::imprimir(){ //funcion para imprimir
 int x;
  cout<<"[ ";
-	for(int i=0;i<tam;i++){
+	for(int i=0;i<tam;i++){ //
  		x=conjunto[i];
 		cout<< x <<",";	 
 	}
