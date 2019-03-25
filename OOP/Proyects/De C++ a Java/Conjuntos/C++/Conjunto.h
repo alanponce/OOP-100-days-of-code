8787e629307b8a6e48fb17aa6058b4b957850b14
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-03-25 12:50:50 -0600

    Update Conjunto.h

--- OOP/Proyects/De C++ a Java/Conjuntos/C++/Conjunto.h
+++ OOP/Proyects/De C++ a Java/Conjuntos/C++/Conjunto.h
@@ -1,27 +1,27 @@
-#include <iostream>
-#ifndef CONJUNTO_H
-#define CONJUNTO_H
+#include <iostream> //incluimos la biblioteca standard iostream
+#ifndef CONJUNTO_H //definimos nuestra clase conjunto
+#define CONJUNTO_H //definimos lconjunto como macro
 
-class Conjunto
+class Conjunto //creamos nuestra clase y la llamamos conjunto
 {
-	private: 
-	int conjunto[50];
-	int tam;
+	private: //declaramos los datos privados 
+	int conjunto[50];//creamos una variable de tipo entero llamada conjunto
+	int tam;//creamos una variable de tipo entero llamada tam
 	 
-	public:
+	public://declaramos datos publicos
 		Conjunto();
 		
-		void Vaciar();
-		void Insertar(int x);
-		void Eliminar(int x);
+		void Vaciar();//declaramos la funcion vaciar
+		void Insertar(int x);//declaramos la duncion insertar para un caracter de tipo entero
+		void Eliminar(int x);//declaramos una funcion eliminar para un caracter de tipo entero
 		
-		bool esVacio();
-		bool Pertenece(int x);
+		bool esVacio();//declaramos una variable de tipo bool llamandola vacio
+		bool Pertenece(int x);//declaramos una variable de tipo bool pertenece para un caracter de tipo entero
 		
-		int tamano();
-		int Mostrar(int posicion);
+		int tamano();//declaramos una variable int que nos muestre el tamano del conjunto
+		int Mostrar(int posicion);//declaramos una variable int que nos muestre la posicion
 		
-		void imprimir();
+		void imprimir();//declaramos la funcion para imprimir
 };
 
 #endif
