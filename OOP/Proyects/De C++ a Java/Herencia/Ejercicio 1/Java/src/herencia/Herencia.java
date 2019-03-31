17efa2556e38b6cc2fb486e78acac63f5ccdbfa0
169870MichelleRincon <47225452+169870MichelleRincon@users.noreply.github.com>
2019-03-31 15:11:30 -0600

    Update Herencia.java

--- OOP/Proyects/De C++ a Java/Herencia/Ejercicio 1/Java/src/herencia/Herencia.java
+++ OOP/Proyects/De C++ a Java/Herencia/Ejercicio 1/Java/src/herencia/Herencia.java
@@ -1,11 +1,11 @@
 
-package herencia;
+package herencia;   //Se utiliza package herencia para relacionar las tres clases
 
-public class Herencia {
+public class Herencia {     //Se crea una clase Herencia con modificador de acceso public
 
-    public static void main(String[] args) {
-        Profesor profesor1 = new Profesor("Juan", "Hernandez", 33);
-        profesor1.setIdProfesor("Prof 22-387-11");
-        profesor1.Show();
+    public static void main(String[] args) {    //Metodo estatico de tipo void con modificador de acceso publico con parametros de aareglo vacio de tipo String
+        Profesor profesor1 = new Profesor("Juan", "Hernandez", 33);     //Se crea un objeto profesor1 de la clase Profesor y se le asignan valores
+        profesor1.setIdProfesor("Prof 22-387-11");      //ingresa el id del profesor y se almacena en IdProfesor
+        profesor1.Show();   //Se llama al metodo show de la clase profesor
     }   
 }
