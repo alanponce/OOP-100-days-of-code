4cb5a8ab89e4dec8d724048ebcdfb366a2990bf0
169870MichelleRincon <47225452+169870MichelleRincon@users.noreply.github.com>
2019-03-31 15:06:44 -0600

    Update Persona.java

--- OOP/Proyects/De C++ a Java/Herencia/Ejercicio 1/Java/src/herencia/Persona.java
+++ OOP/Proyects/De C++ a Java/Herencia/Ejercicio 1/Java/src/herencia/Persona.java
@@ -6,21 +6,21 @@ public class Persona {  //Se crea una clase persona con modificador de acceso pu
     private String LastName;    //Se declara una variable de tipo String y modificador de acceso privado de nombre LastName
     private int age;    //Se declara una variable de tipo entero y modificador de acceso privado de nombre age
     
-    public Persona (String name, String LastName, int age) {    //Constructor de la clase que tiene como argumentos las variable privadas de la clase
+    public Persona (String name, String LastName, int age) {    //Constructor de la clase que tiene como parametros las variable privadas de la clase
         this.name = name;   //Se utiliza this para acceder a la variable privada "oculta" name
         this.LastName = LastName;   //Se utiliza this para acceder a la variable privada "oculta" LastName
         this.age=age;   //Se utiliza this para acceder a la variable privada "oculta" age
     }
     
-    public String getName(){    //Metodo getName de tipo String con modificador de acceso publico sin argumentos
+    public String getName(){    //Metodo getName de tipo String con modificador de acceso publico sin parametros
         return name;    //Regresa el contenido de la variable name
     }
     
-    public String getLastName(){    //Metodo getLastName de tipo String con modificador de acceso publico sin argumentos
+    public String getLastName(){    //Metodo getLastName de tipo String con modificador de acceso publico sin parametros
         return LastName;    //Regresa el contenido de la variable LastName
     }
     
-    public int getAge(){    //Metodo getAge de tipo entero con modificador de acceso publico sin argumentos
+    public int getAge(){    //Metodo getAge de tipo entero con modificador de acceso publico sin parametros
         return age; //Regresa el contenido de la variable age
     }
 }
