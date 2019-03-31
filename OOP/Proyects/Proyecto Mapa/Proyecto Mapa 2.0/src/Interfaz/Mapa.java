cd9bd4d6aab5cbdb9e7fe93179d9e57993c30c61
159994JuanGutierrez <46794406+159994JuanGutierrez@users.noreply.github.com>
2019-03-30 19:10:09 -0600

    Add files via upload
    +Se agrego un metodo para poder cambiar los "espacio" a "+" en la textbox y realizar la busqueda del domicilio.
    +Se agrego un panel que nos muestra el mapa de google maps en la aplicacion.
    +Se agrego una nueva funcionalidad a 2 boton para que puedan mostrar sus mapas en la aplicacion.
    +Se agregaron 2 botones nuevos que interactuan con la nueva funcionalidad del mapa en la aplicacion.

---
+++ OOP/Proyects/Proyecto Mapa/Proyecto Mapa 2.0/src/Interfaz/Mapa.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaz;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
+public class Mapa extends JFXPanel {
+//Variable encargada de renderizar el website
+
+    private WebEngine engine;
+
+    //Constructor de la clase
+    public Mapa() {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                WebView view = new WebView();
+                engine = view.getEngine();
+                setScene(new Scene(view));
+            }
+        });
+        setVisible(true);
+    }
+//Método para cargar la URL de la página web
+
+    public void loadURL(final String url) {
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                String tmp = toURL(url);
+                if (tmp == null) {
+                    tmp = toURL(url);
+                }
+                engine.load(tmp);
+
+            }
+        });
+    }
+
+    private String toURL(String str) {
+        try {
+            return new URL(str).toExternalForm();
+        } catch (MalformedURLException exception) {
+            return null;
+        }
+    }
+}
