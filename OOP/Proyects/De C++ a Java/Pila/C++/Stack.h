ff259c8861ac7954376b22acc0d97071dcc2e954
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-04-01 20:40:24 -0600

    Update Stack.h

--- OOP/Proyects/De C++ a Java/Pila/C++/Stack.h
+++ OOP/Proyects/De C++ a Java/Pila/C++/Stack.h
@@ -1,23 +1,23 @@
-#ifndef STACK_H
-#define STACK_H
+#ifndef STACK_H //definimos nuestra clase stack
+#define STACK_H //definimos stack como macro
 
-typedef int TipoDeDato;
-const int TAM = 50;
+typedef int TipoDeDato;//con el metodo typedef declaramos una variable de tipo entero y la llamamos tipo de dato
+const int TAM = 50; //creamos la variable de tipo entero TAM y la inicializamos en cero
 
-class Stack
+class Stack //creamos la clase llamada stack
 {
-	private: 
-		int TOP; 
-		TipoDeDato arrStack[TAM]; 
+	private: //declaramos los datos privados
+		int TOP; //creamos una variable de tipo entero y la llamamos TOP
+		TipoDeDato arrStack[TAM]; //creamos un arreglo lo llamamos TAM
 		
-  public: 
+  public: //declaramos los datos publicos
 		Stack ();
-		bool Push(TipoDeDato elemento); 
-		TipoDeDato Pop(); 
-		void Empty(); 
-		TipoDeDato Top();
-		bool isEmpty(); 
-		bool isFull(); 
+		bool Push(TipoDeDato elemento); //creamos una funcion push de tipo bool para poder ingresar un elemento
+		TipoDeDato Pop(); //creamos un tipo de dato pop
+		void Empty(); //creamos un metodo void Empty o mejor dicho que no devuelve ningun valor
+		TipoDeDato Top();//creamos un tipo de dato top
+		bool isEmpty(); //creamos una funcion isEmpty de tipo bool
+		bool isFull(); //funcion isFull de tipo bool
 };
 
 #endif
