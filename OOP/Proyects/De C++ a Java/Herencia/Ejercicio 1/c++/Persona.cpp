ab2a352a40770bd386b35e8d2839e76febdf32f1
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-04-04 11:19:40 -0600

    Update Persona.cpp

--- OOP/Proyects/De C++ a Java/Herencia/Ejercicio 1/c++/Persona.cpp
+++ OOP/Proyects/De C++ a Java/Herencia/Ejercicio 1/c++/Persona.cpp
@@ -1,22 +1,22 @@
-#include "Persona.h"
+#include "Persona.h" //incluimos la clase persona
 
-Persona::Persona(char name, char LastName, int age)
+Persona::Persona(char name, char LastName, int age) //declaramos que la funcion persona pertenece a la clase persona y se le asignan valores de tipo char e int que almacenan nombre,apellido y edad
 {
-	this->name = name;
-	this->LastName = LastName;
-	this->age = age;
+	this->name = name;//declaramos un puntero this para recuperar name del objeto oculto por nuestra variable name
+	this->LastName = LastName;//declaramos un puntero this para recuperar lastname del objeto oculto por nuestra variable lastname
+	this->age = age;//declaramos un puntero this para recuperar age del objeto oculto por nuestra variable age
 }
 
-char Persona::getName(){
-	return name;	
+char Persona::getName(){ //declaramos que la funcion getname pertenece a la clase persona y es de tipo char
+	return name; //regresa el nombre
 }
 
-char Persona::getLastName(){
-	return LastName;
+char Persona::getLastName(){ //declaramos que la funcion getlastname pertenece a la clase persona y es de tipo char
+	return LastName;//regresa el apellido
 } 
 
-int Persona::getAge(){
-	return age;
+int Persona::getAge(){ //declaramos que la funcion getage pertenece a la clase persona y es de tipo entero
+	return age;//nos regresa la edad
 } 
 
 
