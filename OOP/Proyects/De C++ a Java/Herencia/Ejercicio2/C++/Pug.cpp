8d0ac0c8dad3006d052f21ec0ab0fbc54d956a08
al169838DiegoAlmanza <48567250+al169838DiegoAlmanza@users.noreply.github.com>
2019-04-03 23:46:47 -0600

    Update Pug.cpp

--- OOP/Proyects/De C++ a Java/Herencia/Ejercicio2/C++/Pug.cpp
+++ OOP/Proyects/De C++ a Java/Herencia/Ejercicio2/C++/Pug.cpp
@@ -1,19 +1,19 @@
-#include "Pug.h"
+#include "Pug.h"//Se incluye la clase Pug
 
-Perros::Pug(char, int , float)
+Perros::Pug(char, int , float)//Constructor que recibe parametros de tipo char, int y float
 {
 	typedef(name, age, weiht);
 	nameOwner = "Unknow";
 }
 
-void Pug::setNameOwner(){
-	this->nameOwner = nameOwner;
+void Pug::setNameOwner(){//Funcion setNameOwner de tipo void
+	this->nameOwner = nameOwner;//this llama la funcion privada nameOwner 
 }
 
-char Pug::getNameOwner(){
-	return nameOwner;
+char Pug::getNameOwner(){//Funcion getNameOwner de tipo char
+	return nameOwner;//Retorna la variable nameOwner
 }
 
-void print(){
-	printf("Dog's name: ", getName(), " with weight ", getWeight(),", Owner's name: ", getNameOwner());
+void print(){//funcion print de tipo void
+	printf("Dog's name: ", getName(), " with weight ", getWeight(),", Owner's name: ", getNameOwner());//Muestra las funciones
 }
