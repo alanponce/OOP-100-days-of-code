ccf23cc7ea87483f4a6c446c0acf344938a4000c
al169838DiegoAlmanza <48567250+al169838DiegoAlmanza@users.noreply.github.com>
2019-04-03 23:40:43 -0600

    Update Perros.h

--- OOP/Proyects/De C++ a Java/Herencia/Ejercicio2/C++/Perros.h
+++ OOP/Proyects/De C++ a Java/Herencia/Ejercicio2/C++/Perros.h
@@ -1,17 +1,17 @@
 #ifndef PERROS_H
 #define PERROS_H
 
-class Perros
+class Perros//Se declara la clase Perros
 {
 	private:
-		char name;
-		int age;
-		float weight;
+		char name;//Se declara la variable privada name de tipo char 
+		int age;//Se declara la variable privada age de tipo int
+		float weight;//Se declara la variable privada weight de tipo float
 	public:
-		Perros(char, int, float);
-		char getName();
-		int getAge();
-		float getWeight();
+		Perros(char, int, float);//Constructor que recibe parametros de tipo char, int y float
+		char getName();//Se declara la funcion getName de tipo char
+		int getAge();//Se declara la funcion getAge de tipo int
+		float getWeight();//Se declara la funcion getWeight de tipo float
 	protected:
 };
 
