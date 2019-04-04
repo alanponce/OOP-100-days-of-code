7923eda57475d7720855a24738cd21723b8cb6a6
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-04-03 21:07:51 -0600

    Update Profesor.h

--- OOP/Proyects/De C++ a Java/Herencia/Ejercicio 1/c++/Profesor.h
+++ OOP/Proyects/De C++ a Java/Herencia/Ejercicio 1/c++/Profesor.h
@@ -1,17 +1,17 @@
-#ifndef PROFESOR_H
-#define PROFESOR_H
-#include "Persona.h"
+#ifndef PROFESOR_H //definimos nuestra clase profesor
+#define PROFESOR_H //definimos profesor como macro
+#include "Persona.h" //incluimos nuestra clase persona
 
-class Profesor : public Persona
+class Profesor : public Persona //definimos nuestra clase profesor usando el modificador de acceso publico
 {
-	private: 
+	private: //utilizamos el modificador de acceso privado
 		char IdProfesor;
-	public:
+	public: //utilizamos el modificador de acceso publico
 		Profesor(char, char, int);
 		void setIdProfesor(char IdProfesor);
 		char getIdProfesor();
 		void show();
-	protected:
+	protected: //utilizamos el modificador de acceso protected
 };
 
 #endif
