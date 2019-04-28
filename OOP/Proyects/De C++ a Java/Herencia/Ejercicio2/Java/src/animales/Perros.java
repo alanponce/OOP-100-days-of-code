1f65e839991a2862fff6588d5a8f9d571a4f2805
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-04-28 17:18:57 -0600

    Update Perros.java

--- OOP/Proyects/De C++ a Java/Herencia/Ejercicio2/Java/src/animales/Perros.java
+++ OOP/Proyects/De C++ a Java/Herencia/Ejercicio2/Java/src/animales/Perros.java
@@ -1,26 +1,26 @@
 
-package animales;
+package animales;//invocamos nuestro paquete animales
 
-public class Perros {
-    private String name;
-    private int age;
-    private float weight;
+public class Perros { //creamos nuestra clase publica perros
+    private String name;//declaramos un dato de tipo string de caracter privado llamado name
+    private int age;//declaramos un dato de tipo entero de caracter privado llamado age
+    private float weight;//declaramos un dato de tipo flotante  de caracter privado llamado weight
     
-    public Perros(String name, int age, float weight){
-        this.name=name;
-        this.age=age;
-        this.weight=weight;
+    public Perros(String name, int age, float weight){//llamamos nuestra  clase perros incluye datos de tipo string,int y float
+        this.name=name; //en esta linea se alm acena el nombre
+        this.age=age;//en esta linea se almacena la edad
+        this.weight=weight;//en esta linea almacenamos el peso
     }
     
-    public String getName(){
-        return name;
+    public String getName(){//declaramos un dato de tipo string de caracter publico llamado getName
+        return name;//muestra el nombre del perro
     }
     
-    public int getAge(){
-        return age;
+    public int getAge(){//declaramos un dato de tipo int de caracter publico llamado getAge
+        return age;//muestra la edad del perro
     }
     
-    public float getWeight(){
-        return weight;
+    public float getWeight(){////declaramos un dato de tipo float de caracter publico llamado getWeight
+        return weight;//muestra el peso del perro
     }
 }
