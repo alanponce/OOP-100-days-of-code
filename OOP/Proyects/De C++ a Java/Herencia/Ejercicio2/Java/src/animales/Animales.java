98fa42fab6ab6afc2053bc798a9a0fe60ef052c5
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-04-28 17:32:47 -0600

    Update Animales.java

--- OOP/Proyects/De C++ a Java/Herencia/Ejercicio2/Java/src/animales/Animales.java
+++ OOP/Proyects/De C++ a Java/Herencia/Ejercicio2/Java/src/animales/Animales.java
@@ -1,11 +1,11 @@
 
-package animales;
+package animales;//creamos un paquete que llamaremos animales
 
-public class Animales {
+public class Animales {//creamos nuestra clase de caracter publico llamandola animales
 
-    public static void main(String[] args) {
-        Pug Dog1 = new Pug("Dixie", 7, (float) 10.35);
-        Dog1.setNameOwner("Michelle Rincon");
-        Dog1.print();
+    public static void main(String[] args) { //creamos nuestra funcion main
+        Pug Dog1 = new Pug("Dixie", 7, (float) 10.35);//creamos un nuevo objeto llamado pug
+        Dog1.setNameOwner("Michelle Rincon");//en esta linea nos permiyte establecer el nombre del propietario del perro
+        Dog1.print();//imprime el nombre del propietario del perro
     }   
 }
