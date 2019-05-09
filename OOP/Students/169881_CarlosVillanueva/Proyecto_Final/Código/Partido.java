3137e9a9b57dd56c6acbd7bb94151a195f93cbc7
169881 Carlos <46772724+Carlos169881@users.noreply.github.com>
2019-05-09 08:40:20 -0600

    Update Partido.java

--- OOP/Students/169881_CarlosVillanueva/Proyecto_Final/Código/Partido.java
+++ OOP/Students/169881_CarlosVillanueva/Proyecto_Final/Código/Partido.java
@@ -1,8 +1,9 @@
 package equipo.de.baloncesto;
 public class Partido {
-    static int Tiempo = 0;
-    static int Cuarto = 1;
-    static int Distancia = 0;
-    static int Puntuacion1 = 0;
-    static int Puntuacion2 = 0;
+    static int Tiempo = 0;//Indica el tiempo transcurrido en minutos.
+    static int Cuarto = 1;//Indica el cuarto en el que se encuentra el partido.
+    static int Distancia = 0;/*Indica la distancia en la que se encuentra el balón, si la distancia es negativa se encuentra dentro de
+    la mitad enemiga, si es negativa se encuentra en tu mitad.*/
+    static int Puntuacion1 = 0;/*Indica la puntuación actual de tu equipo*/
+    static int Puntuacion2 = 0;/*Indica la puntuación actual del equipo contrario*/
 }
