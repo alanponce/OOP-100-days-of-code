4cbcf15359df84d43e25f5d14a699916d6092815
HelloWorld-163953AntonioCarrera <47226264+HelloWorld-163953AntonioCarrera@users.noreply.github.com>
2019-05-14 13:13:05 -0600

    Add files via upload

---
+++ OOP/Students/163953_AntonioCarrera/Proyecto Final/EmpleadoAsalariado.java
@@ -0,0 +1,32 @@
+﻿
+public class EmpleadoAsalariado extends Empleado{
+    
+    private double salario;
+
+//Permite heredar los atributos de la clase empleados
+    public EmpleadoAsalariado(String nombre, String apellido, String nss, double salario){
+        super(nombre, apellido, nss);
+        establecerSalarioSemanal(salario);
+    }
+
+    @Override
+    public double ingresos(){
+        return obtenerSalarioSemanal();
+    }
+    
+//Permite establecer salario semanal
+    public void establecerSalarioSemanal(double sal){
+        salario = sal;
+    }
+    
+//permite obtener Salario semanal
+    public double obtenerSalarioSemanal(){
+        return salario;
+    }
+    
+//reescribe el método
+    @Override
+    public String toString(){
+        return String.format("Empleado asalariado: %s\n%s: $%,.2f", super.toString(), "Salario Semanal", obtenerSalarioSemanal());
+    }
+}
