06a53752e7d30372cd5763dd5f7b2ef327d30875
159971 Robert Mares <44788821+RobertMares@users.noreply.github.com>
2019-05-16 20:22:47 -0600

    Delete WarehouseOperations.java

--- OOP/Students/159971_RobertMares/Final Project/Code/warehousesystem/WarehouseOperations.java
+++
@@ -1,7 +0,0 @@
-
-package warehousesystem;
-
-public interface WarehouseOperations {
-    public boolean addItems(int id, int qty, Stock stock); 
-    public boolean removeItems(int id, int qty, Stock stock);
-}
